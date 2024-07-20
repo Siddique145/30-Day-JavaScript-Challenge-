1e19431a723adb9a060fc6af40ea4e0cfcab97d0
Siddique145 <muhammadsiddique14344@gmail.com>
2024-07-20 16:49:05 -0700

    initial commit

---
+++ Day-1/app.js
@@ -0,0 +1,42 @@
+//Task 1
+var number = 123;
+console.log("Task-1=>", number);
+
+//task 2
+let string = "Hello I am Muhammad Siddique";
+console.log("Task-2=>", string);
+
+//Task 3
+const boolean = true;
+console.log("Task-3=>", boolean);
+
+//task 4
+// Number
+let numberVar = 42;
+console.log(typeof numberVar);
+
+// String
+let stringVar = "Hello, World!";
+console.log(typeof stringVar);
+
+// Boolean
+let booleanVar = true;
+console.log(typeof booleanVar);
+// Object
+let objectVar = { name: "MuhammadSiddique", age: 17 };
+console.log(typeof objectVar);
+// Array
+let arrayVar = [1, 2, 3, 4, 5];
+console.log(typeof arrayVar);
+
+//task 5
+let firstValue = 123;
+console.log("firstvalue", firstValue);
+firstValue = 456;
+console.log("first value after reassigning=>", firstValue);
+
+//task 6
+const first = "I am first Value";
+console.log("First=>", first);
+first = "now I am reassigned value";
+console.log("Reassigned Value of Const", first);
---
+++ Day-2/app.js
@@ -0,0 +1,83 @@
+//Task 1
+
+var one = 1;
+var two = 2;
+var sum = one + two;
+console.log("Sum =>", sum);
+
+//Task 2
+
+var num1 = 5;
+var num2 = 3;
+var subtract = num1 - num2;
+console.log("Subrated value =>", subtract);
+
+//Task 3
+var digit = 10;
+var digit2 = 10;
+var multiply = digit * digit2;
+console.log("Multiplicatec Value=>", multiply);
+
+//Task 4
+var nu1 = 50;
+var nu2 = 5;
+var divide = nu1 / nu2;
+console.log("Divided Value=>", divide);
+
+//Task 5
+var n = 20;
+var n2 = 9;
+var modulas = n % n2;
+console.log("Remainder=>", modulas);
+
+//Task 6
+var a = "Muhammad";
+a += "Siddique";
+console.log("a=>", a);
+
+//Task 7
+var b = 15;
+b -= 10;
+console.log("b=>", b);
+
+//Task 8
+
+var c = 20;
+var c1 = 30;
+var c11 = c < c1;
+console.log("compared vlaue >", c11);
+
+//Task 9
+
+var d = 20;
+var d1 = 50;
+var d11 = d > d1;
+console.log("compared value >", d11);
+
+//Task 10
+var e = 10;
+var f = 10;
+console.log("Task 10", e === f);
+
+//Task 11
+
+var a = 3;
+var b = -2;
+console.log(a > 0 && b > 0);
+
+//Task 12
+
+var a1 = 4;
+var a2 = -10;
+console.log(a1 > 0 || a2 > 0);
+
+//Task 13
+
+var a11 = 5;
+var a22 = -20;
+console.log(a11 != 0 && a22 != 0);
+
+//Task 14
+var age = 26;
+var beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage);
